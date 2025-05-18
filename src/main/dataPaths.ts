6b5c560336184f17015593853d05068094beb814
Oscar Fredriksson <oscar@famfredriksson.com>
2025-05-18 23:13:42 +0200

    cleanup

--- src/main/dataPaths.ts
+++
@@ -1,8 +0,0 @@
-import { app } from 'electron';
-
-const basePath = app.getPath('userData') + '/';
-const tireDataFileName = 'tire-data.json';
-const stintDataFileName = 'stint-data.json';
-
-export const TIRE_DATA_PATH = basePath + tireDataFileName;
-export const STINT_DATA_PATH = basePath + stintDataFileName;
